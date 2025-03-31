6e33c3d5ebedbc3a972ea8186124f307f22f642f
Petra <petra66orii@gmail.com>
2025-03-31 18:02:23 +0100

    Implement profile form in the views

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-# from django.contrib import messages
+from django.contrib import messages
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 # Create your views here.
@@ -20,7 +21,21 @@ def user_profile(request):
     """
     profile = get_object_or_404(UserProfile, user=request.user)
 
-    return render(
-        request, 'userprofile/profile.html',
-        {'profile': profile,
-         })
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(
+                request,
+                'Update failed. Please ensure the form is valid.'
+                )
+    else:
+        form = UserProfileForm(instance=profile)
+
+    context = {
+        'form': form,
+        'on_profile_page': True
+    }
+    return render(request, 'userprofile/profile.html', context)
