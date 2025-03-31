7bc9a0c40a1471dcca8818fb588e63afa2cb690e
Petra <petra66orii@gmail.com>
2025-03-31 18:01:04 +0100

    Update model and migrate

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from django_countries.fields import CountryField
 
 
 # Create your models here.
@@ -13,10 +17,21 @@ class UserProfile(models.Model):
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     street_address1 = models.CharField(max_length=255)
     street_address2 = models.CharField(max_length=255, null=True, blank=True)
-    country = models.CharField(max_length=100)
+    country = CountryField(blank_label='Country', null=True, blank=True)
     county = models.CharField(max_length=100, null=True, blank=True)
     town = models.CharField(max_length=100)
     postcode = models.CharField(max_length=20)
 
     def __str__(self):
         return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()
