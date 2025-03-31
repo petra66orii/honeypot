36ecb06a22eb313cee850c7173710efeea918212
Petra <petra66orii@gmail.com>
2025-03-31 18:39:56 +0100

    Correct URL in ALLOWED_HOSTS

--- honeypot/settings.py
+++ honeypot/settings.py
@@ -33,7 +33,7 @@ DEBUG = 'DEVELOPMENT' in os.environ
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
-    'honeypot-6aa199604c8f.herokuapp.com/'
+    'honeypot-6aa199604c8f.herokuapp.com'
 ]
 
 # Application definition
