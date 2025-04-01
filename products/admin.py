65654df2aa0b876505b5c1f33ce047b3acc50275
Petra <petra66orii@gmail.com>
2025-04-01 23:25:15 +0100

    Register ProductReview model with admin, #15, #9

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, ProductReview
 
 
 # Register your models here.
@@ -24,5 +24,17 @@ class CategoryAdmin(admin.ModelAdmin):
     )
 
 
+class ProductReviewAdmin(admin.ModelAdmin):
+    list_display = ("user", "product", "rating", "approved", "created_on")
+    list_filter = ("approved", "rating")
+    search_fields = ("user__username", "product__name", "review_text")
+    actions = ["approve_reviews"]
+
+    def approve_reviews(self, request, queryset):
+        queryset.update(approved=True)
+    approve_reviews.short_description = "Approve selected reviews"
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(ProductReview, ProductReviewAdmin)
