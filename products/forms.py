721f991aa6211c39d0c136620027c874980e4211
Petra <petra66orii@gmail.com>
2025-04-01 23:20:28 +0100

    Add ReviewForm, #15

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Product, Category
+from .models import Product, Category, ProductReview
 
 
 # Product form borrowed from the Boutique Ado walkthrough project
@@ -19,3 +19,21 @@ class ProductForm(forms.ModelForm):
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = ProductReview
+        fields = ('review_text',
+                  'rating',)
+        widgets = {
+            'review_text': forms.Textarea(attrs={
+                'rows': 9,
+                'placeholder': 'Write your review here...'
+            }),
+            'rating': forms.Select(choices=[
+                (0.5, "0.5 ★"), (1, "1 ★"), (1.5, "1.5 ★"),
+                (2, "2 ★"), (2.5, "2.5 ★"), (3, "3 ★"),
+                (3.5, "3.5 ★"), (4, "4 ★"), (4.5, "4.5 ★"), (5, "5 ★")
+            ]),
+        }
