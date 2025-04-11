f1e325fe00d22560dcc0624a08bff5b795ef1f0d
Petra <petra66orii@gmail.com>
2025-04-11 20:13:22 +0100

    Register Order model with admin, #23

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Order
+
 
 # Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'total_price',)
+
+    fields = ('order_number', 'date', 'first_name', 'last_name'
+              'email', 'phone_number', 'country',
+              'postcode', 'town', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'total_price',)
+
+    list_display = ('order_number', 'date', 'first_name',
+                    'last_name', 'order_total', 'delivery_cost',
+                    'total_price',)
+
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
