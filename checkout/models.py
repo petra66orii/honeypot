23a2c60d78335e4c72a7b64d638988344fb6dd56
Petra <petra66orii@gmail.com>
2025-04-15 15:19:06 +0100

    Edit Order and OrderItem fields and migrate, #23

--- checkout/models.py
+++ checkout/models.py
@@ -1,19 +1,31 @@
+import uuid
 from django.db import models
+
+from django.db.models import Sum
+from django.conf import settings
+
+from django_countries.fields import CountryField
 from userprofile.models import UserProfile
 from products.models import Product
 
 
 # Create your models here.
 class Order(models.Model):
-    order_number = models.CharField(max_length=100, unique=True)
-    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(
+        UserProfile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='orders'
+        )
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
     email = models.EmailField()
     phone_number = models.CharField(max_length=20)
     street_address1 = models.CharField(max_length=255)
     street_address2 = models.CharField(max_length=255, null=True, blank=True)
-    country = models.CharField(max_length=100)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     town = models.CharField(max_length=100)
     county = models.CharField(max_length=100, null=True, blank=True)
     postcode = models.CharField(max_length=20)
@@ -23,20 +35,68 @@ class Order(models.Model):
         decimal_places=2,
         default=0.00
         )
-    order_total = models.IntegerField()
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    order_total = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=False,
+        default=0)
+    total_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=False,
+        default=0)
     stripe_pid = models.CharField(max_length=255)
+
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        Update grand total each time an item is added,
+        accounting for delivery costs.
+        """
+        self.order_total = self.items.aggregate(Sum('item_total'))[
+            'item_total__sum'
+            ] or 0
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            sdp = settings.STANDARD_DELIVERY_PERCENTAGE
+            self.delivery_cost = self.order_total * sdp / 100
+        else:
+            self.delivery_cost = 0
+        self.total_price = self.order_total + self.delivery_cost
+        self.save()
+
+    def save(self, *args, **kwargs):
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.order_number
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    order = models.ForeignKey(
+        Order,
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE,
+        related_name='items'
+        )
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
-    price_at_purchase = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
     item_total = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the item total
+        and update the order total.
+        """
+        self.item_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.order.order_number} - {self.product.name}"
