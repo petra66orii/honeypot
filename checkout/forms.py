fb2bd54572bbae3c3cb329e6d22a739488619d67
Petra <petra66orii@gmail.com>
2025-04-15 13:13:45 +0100

    Add mandatory asterisk(*) to all fields except country field

--- checkout/forms.py
+++ checkout/forms.py
@@ -21,7 +21,6 @@ class OrderForm(forms.ModelForm):
             'last_name': 'Last Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postal Code',
             'town': 'Town or City',
             'street_address1': 'Street Address 1',
@@ -30,10 +29,11 @@ class OrderForm(forms.ModelForm):
         }
 
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
