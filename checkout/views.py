443e46acda6ad2020b8dc4128e8bb52f8d870353
Petra <petra66orii@gmail.com>
2025-04-15 15:18:37 +0100

    Expand on checkout view, #23

--- checkout/views.py
+++ checkout/views.py
@@ -1,80 +1,137 @@
 import stripe
 import json
-from django.shortcuts import render, redirect, HttpResponse
+
+from django.shortcuts import (
+    render, redirect, HttpResponse, get_object_or_404, reverse
+    )
 from django.conf import settings
 from django.contrib import messages
 from django.views.decorators.http import require_POST
+
+from products.models import Product
+from userprofile.forms import EditProfileForm
 from userprofile.models import UserProfile
 from .forms import OrderForm
-# from .models import Order, OrderItem
+from .models import Order, OrderItem
 from bag.contexts import bag_contents
-
-stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 # Create your views here.
+# Checkout view borrowed and adapted from Code Institute's Boutique Ado project
 def checkout(request):
     """
-    A view that renders the checkout page
+    A view that handles the checkout process
     """
-    bag = request.session.get('bag', {})
-    if not bag:
-        messages.error(request, "Your bag is empty.")
-        return redirect('products')
-
-    context = bag_contents(request)
-    total = context['grand_total']
-    stripe_total = round(total * 100)
-
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
 
     if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            order = form.save(commit=False)
-            order.user_profile = request.user.userprofile
-            # Add logic for order number, stripe pid, etc
+        bag = request.session.get('bag', {})
+
+        form_data = {
+            'first_name': request.POST['first_name'],
+            'last_name': request.POST['last_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town': request.POST['town'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
             order.save()
-            if request.user.is_authenticated:
-                profile = request.user.userprofile
-                profile.phone_number = order.phone_number
-                profile.street_address1 = order.street_address1
-                profile.street_address2 = order.street_address2
-                profile.town = order.town_or_city
-                profile.county = order.county
-                profile.postcode = order.postcode
-                profile.country = order.country
-                profile.save()
+            for item_id, item_data in bag.items():
+                try:
+                    product = Product.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_item = OrderItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_item.save()
+                    else:
+                        for quantity in item_data.items():
+                            order_item = OrderItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                            )
+                            order_item.save()
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your bag wasn't found. "
+                        "Please contact us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_bag'))
+
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse(
+                'checkout_success',
+                args=[order.order_number]
+                )
+                )
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
     else:
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(
+                request,
+                "There's nothing in your bag at the moment"
+                )
+            return redirect(reverse('products'))
+
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
         if request.user.is_authenticated:
             try:
-                profile = request.user.userprofile
+                profile = UserProfile.objects.get(user=request.user)
                 form = OrderForm(initial={
                     'first_name': request.user.first_name,
                     'last_name': request.user.last_name,
-                    'email': request.user.email,
+                    'email': profile.user.email,
                     'phone_number': profile.phone_number,
+                    'country': profile.country,
+                    'postcode': profile.postcode,
+                    'town': profile.town,
                     'street_address1': profile.street_address1,
                     'street_address2': profile.street_address2,
-                    'town_or_city': profile.town,
                     'county': profile.county,
-                    'postcode': profile.postcode,
-                    'country': profile.country,
                 })
             except UserProfile.DoesNotExist:
                 form = OrderForm()
         else:
             form = OrderForm()
 
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
+    template = 'checkout/checkout.html'
     context = {
         'form': form,
-        'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
+        'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
     }
-    return render(request, 'checkout/checkout.html', context)
+
+    return render(request, template, context)
 
 
 @require_POST
