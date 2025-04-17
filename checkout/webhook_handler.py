28f11b77f63767ba96fcdd528a20c9d4a3863777
Petra <petra66orii@gmail.com>
2025-04-17 18:11:22 +0100

    Add StripeWH class for webhooks and email confirmation, #25

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,69 @@
+from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+
+from .models import Order
+
+
+class StripeWH_Handler:
+    """Handle Stripe webhooks"""
+
+    def __init__(self, request):
+        self.request = request
+
+    def _send_confirmation_email(self, order):
+        """Send the user a confirmation email"""
+        cust_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+        )
+
+    def handle_event(self, event):
+        """
+        Handle a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle the payment_intent.succeeded webhook from Stripe
+        """
+        intent = event.data.object
+        pid = intent.id
+        print(f"Stripe PID from webhook: {pid}")
+
+        try:
+            # Retrieve the order using the Stripe PaymentIntent ID
+            order = Order.objects.get(stripe_pid=pid)
+            self._send_confirmation_email(order)
+            return HttpResponse(
+                content=f'Webhook received: {event["type"]} | '
+                'SUCCESS: Sent confirmation email',
+                status=200)
+        except Order.DoesNotExist:
+            print(f"Order with stripe_pid={pid} not found in the database.")
+            return HttpResponse(
+                content=f'Webhook received: {event["type"]} | '
+                'ERROR: Order not found',
+                status=404)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle the payment_intent.payment_failed webhook from Stripe
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
