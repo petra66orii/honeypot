e402e4867a8e341f70649354f81dcfe735c9f863
Petra <petra66orii@gmail.com>
2025-04-19 13:08:41 +0100

    Create ProductCarousel and Testimonial models and migrate, #38

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ProductCarousel, Testimonial
 
-# Register your models here.
+admin.site.register(ProductCarousel)
+admin.site.register(Testimonial)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class ProductCarousel(models.Model):
+    """
+    Model to represent a product in the carousel.
+    """
+    name = models.CharField(max_length=255)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Testimonial(models.Model):
+    name = models.CharField(max_length=100)
+    text = models.TextField()
+    rating = models.PositiveIntegerField(default=5)
+
+    def __str__(self):
+        return f"{self.name} ({self.rating}⭐)"
