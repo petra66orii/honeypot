964105b7c904d2f3bca3c0cca860866743b0bfeb
Petra <petra66orii@gmail.com>
2025-04-19 13:09:43 +0100

    Add view to display products and testimonials, #38

--- home/views.py
+++ home/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 
+from .models import ProductCarousel, Testimonial
+
 
 # Create your views here.
+def carousel(queryset, n):
+    """
+    Turns a queryset into blocks of n items
+    (e.g. 3 products per carousel slide).
+    """
+    block = []
+    for obj in queryset:
+        block.append(obj)
+        if len(block) == n:
+            yield block
+            block = []
+    if block:
+        yield block
+
+
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+    product_carousel = ProductCarousel.objects.all()
+    testimonials = Testimonial.objects.all()
+
+    product_blocks = list(carousel(product_carousel, 3))
+    testimonial_blocks = list(carousel(testimonials, 3))
+
+    return render(request, "home/index.html", {
+        "product_blocks": product_blocks,
+        "testimonial_blocks": testimonial_blocks,
+    })
