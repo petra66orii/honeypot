7c7d1e084adab044894218a512e536219d7e1f36
Petra <petra66orii@gmail.com>
2025-04-22 21:30:37 +0100

    Modify contexts if bag contains discounted items, #33

--- bag/contexts.py
+++ bag/contexts.py
@@ -14,8 +14,14 @@ def bag_contents(request):
 
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
+    for item_id, item_data in bag.items():
         product = get_object_or_404(Product, pk=item_id)
+
+        if isinstance(item_data, dict):
+            quantity = item_data.get('quantity', 1)
+        else:
+            quantity = item_data
+
         total += quantity * product.price
         product_count += quantity
         bag_items.append({
