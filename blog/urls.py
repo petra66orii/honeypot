34877903eeb0d22f2396a29a32e299221f340acd
Petra <petra66orii@gmail.com>
2025-04-23 20:52:26 +0100

    Add comment management URLs

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.BlogPostList.as_view(), name='blog'),
     path('create/', views.PostCreateView.as_view(), name='create_post'),
-    path('<slug:slug>/', views.PostDetailView.as_view(), name='post'),
     path('<slug:slug>/edit', views.PostUpdateView.as_view(), name='edit_post'),
     path('<slug:slug>/delete', views.PostDeleteView.as_view(),
          name='delete_post'),
@@ -12,4 +11,20 @@ urlpatterns = [
          views.PostDetailView.edit_comment, name='edit_comment'),
     path('<slug:slug>/delete_comment/<int:comment_id>',
          views.PostDetailView.delete_comment, name='delete_comment'),
+    path(
+        'comment_management/',
+        views.comment_management,
+        name='comment_management'
+        ),
+    path(
+        'approve_comment/<int:comment_id>/',
+        views.approve_comment,
+        name='approve_comment'
+        ),
+    path(
+        'admin_delete_comment/<int:comment_id>/',
+        views.admin_delete_comment,
+        name='admin_delete_comment'
+        ),
+    path('<slug:slug>/', views.PostDetailView.as_view(), name='post'),
 ]
