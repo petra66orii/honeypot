e157910fff0fe59d8de74a3cc883acf523e15428
Petra <petra66orii@gmail.com>
2025-04-23 12:13:26 +0100

    Register models with admin, #26

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import BlogPost, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+
+
+# This class was borrowed and adapted from Codestar Blog project
+@admin.register(BlogPost)
+class BlogPostAdmin(SummernoteModelAdmin):
+    """
+    Lists fields for display in admin, fields for search,
+    field filters, fields to prepopulate and rich-text editor.
+    """
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Lists fields for display in admin
+    """
+    list_display = ("post", "user", "content", "created_at")
