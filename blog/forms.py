e396bf73f92e532b72f2ef11a4cbd5140e77b24c
Petra <petra66orii@gmail.com>
2025-04-23 19:13:51 +0100

    Add Create and Edit posts forms, #28

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,47 @@
-from .models import Comment
 from django import forms
+from django_summernote.widgets import SummernoteWidget
+
+from .models import BlogPost, Comment
+
+
+class CreatePost(forms.ModelForm):
+    """
+    Form class for users to create a post
+    """
+    class Meta:
+        """
+        Specify the django model and order of the fields
+        """
+        model = BlogPost
+        fields = ['title',
+                  'content',
+                  'status',
+                  'excerpt',
+                  'featured_image',
+                  ]
+        widgets = {
+            'content': SummernoteWidget(),
+        }
+
+
+class EditPost(forms.ModelForm):
+    """
+    Form class for users to edit a post
+    """
+    class Meta:
+        """
+        Specify the django model and order of the fields
+        """
+        model = BlogPost
+        fields = ['title',
+                  'content',
+                  'status',
+                  'excerpt',
+                  'featured_image',
+                  ]
+        widgets = {
+            'content': SummernoteWidget(),
+        }
 
 
 class CommentForm(forms.ModelForm):
