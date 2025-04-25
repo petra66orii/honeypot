69e8cf2de89fc255d4c43e998cd2a8bc2443ade4
Petra <petra66orii@gmail.com>
2025-04-25 13:35:22 +0100

    Add docstrings to classes

--- home/models.py
+++ home/models.py
@@ -5,6 +5,11 @@ from django.db import models
 class ProductCarousel(models.Model):
     """
     Model to represent a product in the carousel.
+    Each product has a name and an image.
+
+    Attributes:
+        name (str): The name of the product.
+        image (ImageField): The image of the product.
     """
     name = models.CharField(max_length=255)
     image = models.ImageField(null=True, blank=True)
@@ -14,6 +19,14 @@ class ProductCarousel(models.Model):
 
 
 class Testimonial(models.Model):
+    """
+    Model to represent a testimonial.
+    Each testimonial has a name, text, and rating.
+    Attributes:
+        name (str): The name of the person giving the testimonial.
+        text (str): The text of the testimonial.
+        rating (int): The rating given by the person, default is 5.
+    """
     name = models.CharField(max_length=100)
     text = models.TextField()
     rating = models.PositiveIntegerField(default=5)
