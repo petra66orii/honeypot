029aef821c66312f85f0d2434eedc038c572db20
Petra <petra66orii@gmail.com>
2026-01-26 16:29:22 +0000

    Install django_filters

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, permissions, filters, status
 from rest_framework.response import Response
 from rest_framework.exceptions import PermissionDenied
-from rest_framework import DjangoFilterBackend
+from django_filters.rest_framework import DjangoFilterBackend
 from django.db.models import Avg
 
 from .models import Category, Product, ProductReview
